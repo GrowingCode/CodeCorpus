e22c2fa23f376b9e723ff695f60f0baa82648b10
yangy <yangy@192.168.1.104>
2019-05-25 01:49:23 +0800

    small change.

--- CodeCorpus/src/environment/EnvSetUp.java
+++ CodeCorpus/src/environment/EnvSetUp.java
@@ -1,5 +1,29 @@
 package environment;
 
-public class EnvSetUp {
+import java.io.File;
 
+import util.FileUtil;
+
+public class EnvSetUp {
+	
+	public static final String user_home = System.getProperty("user.home");
+	public static final String witness = user_home + "/YYXWitness";
+	public static final String data = user_home + "/YYXData";
+	
+	public static void main(String[] args) {
+		File witness_dir = new File(witness);
+		if (witness_dir.exists()) {
+			FileUtil.DeleteFile(witness_dir);
+		}
+		witness_dir.mkdirs();
+		
+		File data_dir = new File(data);
+		if (data_dir.exists()) {
+			FileUtil.DeleteFile(data_dir);
+		}
+		data_dir.mkdirs();
+		
+		
+	}
+	
 }
