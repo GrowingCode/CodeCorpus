e8178d0de4db9bfa7e71ff9ee4f2fa7243cb96e8
yangy <yangy@192.168.1.104>
2019-05-25 11:33:11 +0800

    small change.

---
+++ CodeCorpus/src/process/PreProcessRawProject.java
@@ -0,0 +1,16 @@
+package process;
+
+public class PreProcessRawProject {
+	
+	public static final String user_home = System.getProperty("user.home");
+	public static final String unzip_dir = user_home + "/PPYYXUnzip";
+	
+	public static void PreProcessOneProject() {
+		
+	}
+	
+	public static void PreProcess() {
+		
+	}
+	
+}
