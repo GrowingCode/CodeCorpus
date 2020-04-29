debc9c66fab30f5b4906002081c8694edcefc8dd
yangy <yangy@DESKTOP-SK2FI36>
2020-04-30 06:25:53 +0800

    small change.

---
+++ CodeCorpus/all_projects/test_project0/ktest/YYX_S_T_R_A_N_G_E_Specified_Valid_File__P_R_E_F_I_X_HeiSimpleTest.java
@@ -0,0 +1,14 @@
+package ktest
+
+public class SimpleTest {
+	
+	public void test() {
+		int i=0;
+		System.out.println(i++);
+//		String s = "%\n%";
+//		System.out.println(s);
+//		char c = '@';
+//		System.out.println(c);
+	}
+	
+}
