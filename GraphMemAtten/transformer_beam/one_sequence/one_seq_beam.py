526e1457c76759edbc8b548d3ff0bb5dbbb4a7a8
Yixiao Yang <yangyixiaofirst@163.com>
2020-11-04 16:46:39 +0800

    small change.

--- GraphMemAtten/transformer_beam/one_sequence/one_seq_beam.py
+++ GraphMemAtten/transformer_beam/one_sequence/one_seq_beam.py
@@ -2,9 +2,10 @@ import tensorflow as tf
 from utils.cartesian_util import batch_cartesian_add_each_scalar_in_vect,\
   batch_cartesian_concat_one_dim_vect_and_each_scalar_in_vect
 from meta_info.non_hyper_constant import int_type, float_type
-from meta_info.hyper_parameter import oracle_mem_len, top_ks, n_layer
+from meta_info.hyper_parameter import oracle_mem_len, top_ks, n_layer,\
+  oracle_tgt_len
 from transformer.model import transformer
-from utils.meta_util import get_one_layer_varied_memory_shape_in_while_loop
+from utils.meta_util import get_varied_memory_shape_in_while_loop
 
 
 class OneSeqBeam:
@@ -12,7 +13,24 @@ class OneSeqBeam:
   def __init__(self):
     pass
   
-  def __call__(self, whole_seq, skt_token_split):
+  def __call__(self, mems_before_last, last_token, whole_seq, skt_token_split):
+    
+    ''' generate stored memory for whole sequence '''
+    seq_len = tf.shape(whole_seq)[0]
+    
+    def mem_gen_cond(idx, *_):
+      return tf.less(idx, seq_len)
+    
+    def mem_gen_body(idx, *all_mems):
+      seq_part_size = tf.minimum(idx + oracle_tgt_len, seq_len) - idx
+      dec_inp = tf.slice(whole_seq, [idx, 0], [seq_part_size, -1])
+      target = tf.zeros_like(dec_inp) - 1
+      
+      new_all_mems = all_mems
+      
+      probs, predictions, _, new_mems = transformer(dec_inp, target, mems, is_training=0, mem_len=oracle_mem_len)
+      
+      return (idx + oracle_tgt_len, *new_all_mems)
     
     def osb_cond(i, i_len, *_):
       return tf.less(i, i_len)
@@ -27,6 +45,11 @@ class OneSeqBeam:
   
   def infer(self, last_token, mems_before_last, steps):
     
+    ''' here mems_before_last shape must be [n_layer memory_length 1 feature_size] '''
+    ''' here mems_before_last shape should be extended to [n_layer memory_length batch_size feature_size] '''
+    for i in range(n_layer):
+      mems_before_last[i] = tf.tile(mems_before_last[i], [1, top_ks[-1], 1])
+    
     def infer_cond(i, i_len, *_):
       return tf.less(i, i_len)
     
@@ -37,7 +60,7 @@ class OneSeqBeam:
       ''' l_token shape should be [1, batch_size] '''
       mems = list(mems_tuple)
       ''' mems shape should be [n_layer memory_length batch_size feature_size] '''
-      probs, predictions, _, new_mems = transformer(l_token, -1, mems, is_training=0, mem_len=oracle_mem_len)
+      probs, predictions, _, new_mems = transformer(l_token, tf.zeros_like(l_token)-1, mems, is_training=0, mem_len=oracle_mem_len)
       ''' probs          should be [1, batch_size, top_ks[-1]] '''
       ''' predictions    should be [1, batch_size, top_ks[-1]] '''
       r_probs = tf.squeeze(probs, [0])
@@ -64,7 +87,7 @@ class OneSeqBeam:
     i_len = tf.constant(steps, int_type)
     probs = tf.zeros([top_ks[-1]], float_type)
     ens = tf.zeros([top_ks[-1], 1], int_type) - 1
-    mems_shapes = get_one_layer_varied_memory_shape_in_while_loop()
+    mems_shapes = get_varied_memory_shape_in_while_loop()
     _, _, probs, ens, *_ = tf.while_loop(infer_cond, infer_body, loop_vars=[i, i_len, probs, ens, last_token, *mems_before_last], shape_invariants=[tf.TensorShape(()), tf.TensorShape(()), tf.TensorShape([top_ks[-1]]), tf.TensorShape([top_ks[-1], None]), tf.TensorShape([1, top_ks[-1]]), *mems_shapes], parallel_iterations=1)
     ens = tf.slice(ens, [0, 1], [-1, -1])
     return ens
--- GraphMemAtten/utils/meta_util.py
+++ GraphMemAtten/utils/meta_util.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from meta_info.hyper_parameter import n_layer, d_model, top_ks
 
 
-def get_one_layer_varied_memory_shape_in_while_loop():
+def get_varied_memory_shape_in_while_loop():
   mems_shapes = []
   for _ in range(n_layer):
     mems_shapes.append(tf.TensorShape([None, top_ks[-1], d_model]))
