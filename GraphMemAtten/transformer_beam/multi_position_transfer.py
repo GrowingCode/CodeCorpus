d64ab6111625e68b688577a1cc46a206caecc96d
yangy <yangy@DESKTOP-GKJ2OL2>
2020-11-07 02:53:19 +0800

    small change.

--- GraphMemAtten/transformer_beam/multi_position_transfer.py
+++ GraphMemAtten/transformer_beam/multi_position_transfer.py
@@ -1,25 +1,22 @@
 import tensorflow as tf
+from utils.initialize_util import random_uniform_variable_initializer
+from meta_info.hyper_parameter import multi_infer_num, d_embed
 
 
 class MultiPositionTransfer():
   
   def __init__(self):
+    self.multi_transfer_parameters = tf.Variable(random_uniform_variable_initializer(258, 578, [multi_infer_num, d_embed, d_embed]))
     
-    pass
-  
   def transfer(self, positions, outputs):
     ''' positions shape: [target_length, batch_size] '''
     ''' positions need to transfer to positions embedding '''
-    ''' positions embedding shape: [target_length, batch_size, feature_size] '''
-    tf.nn.embedding_lookup(lookup_table, x)
+    ''' positions embedding shape: [target_length, batch_size, feature_size, feature_size] '''
+    positions_embedding = tf.nn.embedding_lookup(self.multi_transfer_parameters, positions)
     
     ''' outputs shape: [target_length, batch_size, feature_size] '''
-    transfer_mat = self.multi_transfer_parameters[i]
-    t_h = tf.matmul(h, transfer_mat)
-    
-    ''' outputs and positions embedding should be each matmul '''
-    
-    pass
+    transferred_outputs = tf.einsum('ibde,ibd->ibe', positions_embedding, outputs)
+    return transferred_outputs
   
   
 
