b3c6550f66bcbdeb9dabeb6615e9032364bedb99
yangy <yangy@DESKTOP-GKJ2OL2>
2020-11-08 04:53:50 +0800

    small change.

--- GraphMemAtten/meta_info/non_hyper_constant.py
+++ GraphMemAtten/meta_info/non_hyper_constant.py
@@ -5,4 +5,18 @@ bool_type = tf.bool
 float_type = tf.float32
 int_type = tf.int32
 
+uniform_range = 0.1
+normal_stddev = 0.02
+proj_stddev = 0.01
+
+uniform_initializer = tf.keras.initializers.random_uniform(
+          minval=-uniform_range,
+          maxval=uniform_range,
+          seed=None)
+normal_initializer = tf.compat.v1.initializers.random_normal(
+          stddev=normal_stddev,
+          seed=None)
+proj_initializer = tf.compat.v1.initializers.random_normal(
+          stddev=0.01,
+          seed=None)
 
--- GraphMemAtten/utils/initialize_util.py
+++ GraphMemAtten/utils/initialize_util.py
@@ -1,13 +1,24 @@
-from metas.hyper_settings import initialize_range, initialize_seed_base
-from metas.non_hyper_constants import float_type
 import tensorflow as tf
+from meta_info.non_hyper_constant import float_type, uniform_range,\
+  normal_stddev
 
 variable_initializer_seed = 13
 
-def random_uniform_variable_initializer(mt, bs, shape, ini_range=initialize_range):
+def random_uniform_variable_initializer(shape, mt=0, bs=0, ini_range=uniform_range):
   if ini_range == 0.0:
     return zero_variable_initializer(shape)
-  return tf.random.uniform(shape, minval=-ini_range, maxval=ini_range, dtype=float_type, seed=mt*variable_initializer_seed+bs+initialize_seed_base)
+  seed = mt*variable_initializer_seed+bs
+  if mt == 0 and bs == 0:
+    seed = None
+  return tf.random.uniform(shape, minval=-ini_range, maxval=ini_range, dtype=float_type, seed=seed)
+
+def random_normal_variable_initializer(shape, mt=0, bs=0, ini_stddev=normal_stddev):
+  if normal_stddev == 0.0:
+    return zero_variable_initializer(shape)
+  seed = mt*variable_initializer_seed+bs
+  if mt == 0 and bs == 0:
+    seed = None
+  return tf.random.normal(shape, stddev=ini_stddev, dtype=float_type, seed=seed)
 
 def one_variable_initializer(shape):
   return tf.ones(shape, dtype=float_type)
