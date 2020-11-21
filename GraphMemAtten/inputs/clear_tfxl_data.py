c39b6bcd5b86493d0b3783e1969231a8070a164b
yangy <yangy@DESKTOP-GKJ2OL2>
2020-11-22 01:47:46 +0800

    small change.

--- GraphMemAtten/inputs/clear_tfxl_data.py
+++ GraphMemAtten/inputs/clear_tfxl_data.py
@@ -5,9 +5,13 @@ from meta_info.hyper_parameter import train_tfxl_tfrecord, test_tfxl_tfrecord
 if __name__ == '__main__':
   if os.path.exists(train_tfxl_tfrecord):
     os.remove(train_tfxl_tfrecord)
+    print("tfxl_record " + train_tfxl_tfrecord + " removed!")
     
   if os.path.exists(test_tfxl_tfrecord):
     os.remove(test_tfxl_tfrecord)
+    print("tfxl_record " + test_tfxl_tfrecord + " removed!")
+    
+    
     
     
 
