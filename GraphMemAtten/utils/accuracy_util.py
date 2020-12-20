aefcf9c20ca3589b8b33674599265606e3678685
yangy <yangy@59.66.126.224>
2020-12-20 23:51:44 +0800

    small change.

--- GraphMemAtten/utils/accuracy_util.py
+++ GraphMemAtten/utils/accuracy_util.py
@@ -13,17 +13,26 @@ def compute_unit_expand_accuracy_of_sequences(unit_expand_base, unit_expand_star
   np_seq_list = [tf_seq.numpy() for tf_seq in tf_seq_list]
   l_size = len(np_seq_list)
   
-  each_pos_accurate_count = []
-  each_pos_sub_unit_size = []
-  
   np_oracle_en_seq = raw_oracle_computed_en_seq.numpy()
   np_oracle_valid_mask = oracle_valid_mask.numpy()
   
   infer_size = np.size(np_seq_list[0])
   oracle_size = np.size(np_oracle_en_seq)
   r_size = min(infer_size, oracle_size)
+  
+  max_epos_right = 0
+  max_whole_right = 0
+  
+  tpk_idx = 0
+  f_each_acc = []
+  f_whole_acc = []
+  f_count = -1
+  
   for i in range(l_size):
     nsl = np_seq_list[i]
+    temp_pos_accurate_count = 0
+    temp_pos_sub_unit_size = 0
+    
     for j in range(r_size):
       if np_oracle_valid_mask[j]:
         infer_en = nsl[j]
@@ -31,72 +40,31 @@ def compute_unit_expand_accuracy_of_sequences(unit_expand_base, unit_expand_star
         infer_seq = get_unit_expand_sequence(unit_expand_base, unit_expand_start, unit_expand_end, infer_en)
         oracle_seq = get_unit_expand_sequence(unit_expand_base, unit_expand_start, unit_expand_end, oracle_en)
         pos_acc, pos_sub_unit_size = compare_two_sequences(infer_seq, oracle_seq)
-        each_pos_accurate_count.append(pos_acc)
-        each_pos_sub_unit_size.append(pos_sub_unit_size)
-  
-  positive_idx = tf.where(oracle_valid_mask > 0)
-#   print("tf.shape(tf.gather(raw_oracle_computed_en_seq, positive_idx)):" + str(tf.shape(tf.gather(raw_oracle_computed_en_seq, positive_idx))))
-  oracle_computed_en_seq = tf.squeeze(tf.gather(raw_oracle_computed_en_seq, positive_idx), axis=1)
-  computed_en_seqs = tf.squeeze(tf.gather(raw_computed_en_seqs, positive_idx, axis=1), axis=2)
-  
-#   print("tf.shape(oracle_computed_en_seq):" + str(tf.shape(oracle_computed_en_seq)))
-#   print("tf.shape(computed_en_seqs):" + str(tf.shape(computed_en_seqs)))
-#   print("oracle_computed_en_seq:" + str(oracle_computed_en_seq))
-#   print("computed_en_seqs:" + str(computed_en_seqs))
-  
-  e_lens = tf.ones_like(oracle_computed_en_seq)
-  eq_all_lens_int = tf.reduce_sum(e_lens)
-  e_lens = tf.cast(e_lens, float_type)
-  eq_all_lens = tf.reduce_sum(e_lens)
-  
-  def compute_acc_cond(i, i_len, *_):
-    return i < i_len
-  
-  def compute_acc_body(i, i_len, epos_acc, whole_acc):
-    one_computed_en_seq = computed_en_seqs[i]
-    tc = tf.zeros([tf.shape(oracle_computed_en_seq)[-1] - tf.shape(one_computed_en_seq)[-1]], int_type) - 1
-    r_one_computed_en_seq = tf.concat([one_computed_en_seq, tc], axis=0)
+        temp_pos_accurate_count += pos_acc
+        temp_pos_sub_unit_size += pos_sub_unit_size
     
-    eq = tf.cast(tf.equal(r_one_computed_en_seq, oracle_computed_en_seq), float_type)
-    eq_lens = tf.reduce_sum(eq * e_lens)
-#     eq_all_acc = tf.reduce_sum(eq)
-#     eq_all_count = tf.cast(tf.shape(eq)[-1], float_type)
     if compute_one_whole:
-      epos_right = eq_lens / eq_all_lens
-      whole_right = tf.cast(tf.equal(eq_lens, eq_all_lens), float_type)
+      epos_right = temp_pos_accurate_count / temp_pos_sub_unit_size
+      whole_right = float(temp_pos_accurate_count == temp_pos_sub_unit_size)
+      f_count = 1
     else:
-      epos_right = eq_lens
-      whole_right = tf.cast(tf.equal(eq_lens, eq_all_lens), float_type) * eq_all_lens
+      epos_right = temp_pos_accurate_count
+      whole_right = float(temp_pos_accurate_count == temp_pos_sub_unit_size) * temp_pos_sub_unit_size
+      f_count = temp_pos_sub_unit_size
     
-    epos_r_acc = tf.maximum(epos_acc[-1], epos_right)
-    whole_r_acc = tf.maximum(whole_acc[-1], whole_right)
+    max_epos_right = max(max_epos_right, epos_right)
+    max_whole_right = max(max_whole_right, whole_right)
     
-    epos_acc = tf.concat([epos_acc, [epos_r_acc]], axis=0)
-    whole_acc = tf.concat([whole_acc, [whole_r_acc]], axis=0)
-    
-    return i+1, i_len, epos_acc, whole_acc
+    assert tpk_idx < len(top_ks)
+    if i == top_ks[tpk_idx]:
+      f_each_acc.append(max_epos_right)
+      f_whole_acc.append(max_whole_right)
+      tpk_idx = tpk_idx + 1
   
-  n = tf.shape(computed_en_seqs)[0]
-  each_acc = tf.zeros([1], float_type)
-  whole_acc = tf.zeros([1], float_type)
-  _, _, each_acc, whole_acc = tf.while_loop(compute_acc_cond, compute_acc_body, [0, n, each_acc, whole_acc], [tf.TensorShape(()), tf.TensorShape(()), tf.TensorShape([None]), tf.TensorShape([None])])
+  assert len(top_ks) == len(f_each_acc)
+  assert len(top_ks) == len(f_whole_acc)
   
-  f_each_acc = tf.zeros([0], float_type)
-  f_whole_acc = tf.zeros([0], float_type)
-  acc_len = tf.shape(each_acc)[-1]
-  tpk_len = len(top_ks)
-  for i in range(tpk_len):
-    tpk = top_ks[i]
-    r_sel = tf.minimum(tpk, acc_len-1)
-    f_each_acc = tf.concat([f_each_acc, [each_acc[r_sel]]], axis=0)
-    f_whole_acc = tf.concat([f_whole_acc, [whole_acc[r_sel]]], axis=0)
-  
-  if compute_one_whole:
-    f_count = tf.constant(1, int_type)
-  else:
-    f_count = eq_all_lens_int
-  
-  return f_each_acc, f_whole_acc, f_count
+  return tf.convert_to_tensor(f_each_acc), tf.convert_to_tensor(f_whole_acc), tf.convert_to_tensor(f_count)
 
 
 def get_unit_expand_sequence(unit_expand_base, unit_expand_start, unit_expand_end, en):
