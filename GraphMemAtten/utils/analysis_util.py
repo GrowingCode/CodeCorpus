b34f01080ef5de8fb0050b6c75e52bbe196e033a
Yixiao Yang <yangyixiaofirst@163.com>
2021-04-09 18:04:28 +0800

    small change.

--- GraphMemAtten/utils/analysis_util.py
+++ GraphMemAtten/utils/analysis_util.py
@@ -1,7 +1,42 @@
 
 
+
+
+
+
+
+
+
+
 def get_first_param_right_but_second_param_wrong(first_infer, first_oracle, second_infer, second_oracle):
   pass
+  for fi, fo, si, so in zip(first_infer, first_oracle, second_infer, second_oracle):
+    
+    pass
 
+
+def statistic_acc(ai, ao):
+  for sep_ai, sep_ao in zip(ai, ao):
+    islist1 = isinstance(sep_ai, list)
+    islist2 = isinstance(sep_ao, list)
+    assert islist1 == islist2
+    islist = islist1 and islist2
+    if islist:
+      sai_size = len(sep_ai)
+      sao_size = len(sep_ao)
+      for i in range(sao_size):
+        o_en = sep_ao[i]
+        if i < sai_size:
+          ;
+        else:
+          ;
+      
+      pass
+    else:
+      sep_ai
+      sep_ao
+      pass
+    pass
+  
 
 
