a703ace783459fb8aadb4b592b4ec8e692806859
yangy <yangy@DESKTOP-PV86UN0>
2021-10-08 08:28:32 +0800

    small change.

--- CodeCorpus/all_projects/test_project0/ktest/ObjectMessageTest.java
+++ CodeCorpus/all_projects/test_project0/ktest/ObjectMessageTest.java
@@ -14,7 +14,7 @@
  * See the license for the specific language governing permissions and
  * limitations under the license.
  */
-package test;
+package ktest;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
